4a3d2b3e83501feb856fc22cba25f21d2cf9d191
PedrodeAndradecf <pedro.cfandrade@gmail.com>
2026-07-22 17:46:14 -0300

    config

---
+++ src/main/java/Backend/Estudo/SpringRestaurante/SpringRestauranteApplication.java
@@ -0,0 +1,13 @@
+package Backend.Estudo.SpringRestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestauranteApplication.class, args);
+	}
+
+}
